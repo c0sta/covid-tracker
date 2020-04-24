cff4512884abba72104f2c4a3aa4bf59dc345733
Gabriel <gabrielcostamoura1@gmail.com>
2020-04-24 18:42:01 -0300

    :sparkles: App
    Just added the components in the main file and did the side effects

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { Cards, Chart, CountryPicker } from "./components/index";
+import { fetchData } from "./services/api";
+import "./App.module.css";
 
 function App() {
+  const [data, setData] = React.useState({});
+
+  React.useEffect(() => {
+    const fetch = () =>
+      fetchData().then(
+        (data) => setData({ ...data, data }) & console.log("App log: ", data)
+      );
+    fetch();
+  }, []);
   return (
-    <div className="App">
-      <h1>fuck corona-chan</h1>
+    <div className="container">
+      <Cards data={data} />
+      <Chart />
+      <CountryPicker />
     </div>
   );
 }
