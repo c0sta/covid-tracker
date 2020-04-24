794e957e3054c8871885410a3a62b058a46e70f5
Gabriel <gabrielcostamoura1@gmail.com>
2020-04-24 18:41:09 -0300

    :art: exports
    Created index file to centralize components exports

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { Cards } from "./Cards/Cards.component";
+export { Card } from "./Card/Card.component";
+export { Chart } from "./Chart/Chart.component";
+export { CountryPicker } from "./CountryPicker/CountryPicker.component";
