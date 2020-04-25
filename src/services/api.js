222aa8f0f79a8dd9e92c485ac1dc4f98187827fc
Gabriel <gabrielcostamoura1@gmail.com>
2020-04-24 23:51:50 -0300

    :sparkles: service
    added 2 new fetch functions in api.js

--- src/services/api.js
+++ src/services/api.js
@@ -17,3 +17,27 @@ export const fetchData = async () => {
     console.log(error);
   }
 };
+
+export const fetchDailyData = async () => {
+  try {
+    const { data } = await axios.get(`${url}/daily`);
+    const modifiedData = data.map((dailyData) => ({
+      confirmed: dailyData.confirmed.total,
+      deaths: dailyData.deaths.total,
+      date: dailyData.reportDate,
+    }));
+
+    return modifiedData;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const countries = async () => {
+  try {
+    const response = await axios.get(`${url}/countries`);
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+};
