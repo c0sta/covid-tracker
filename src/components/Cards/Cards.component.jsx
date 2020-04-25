d980f75a8db165b75ce32ebc0fb0e71ebf9fcdfd
Gabriel <gabrielcostamoura1@gmail.com>
2020-04-24 23:50:42 -0300

    :sparkles:
    Added Cards component styles & added some props to components

--- src/components/Cards/Cards.component.jsx
+++ src/components/Cards/Cards.component.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Grid } from "@material-ui/core";
 import { Card } from "../index";
 import styles from "./Cards.module.css";
+import cx from "classnames";
 
 export function Cards({ data: { confirmed, recovered, deaths, lastUpdate } }) {
   console.log(confirmed);
@@ -10,24 +11,32 @@ export function Cards({ data: { confirmed, recovered, deaths, lastUpdate } }) {
   }
   return (
     <div className={styles.container}>
-      <Grid container spacing={3} justify="center">
+      <Grid
+        container
+        spacing={3}
+        className={cx(styles.cards)}
+        justify="space-around"
+      >
         <Card
           title="Confirmed cases"
           value={confirmed.value}
           description="Number of confirmed cases"
           lastUpdate={lastUpdate}
+          className={cx(styles.infected)}
         />
         <Card
           title="Recovered cases"
           value={recovered.value}
           description="Number of recovered cases"
           lastUpdate={lastUpdate}
+          className={cx(styles.recovered)}
         />
         <Card
           title="Deaths"
           value={deaths.value}
           description="Number of deaths"
           lastUpdate={lastUpdate}
+          className={styles.deaths}
         />
       </Grid>
     </div>
