4a26592f3c432f92a3d01ce193bdf9309acb4914
Gabriel <gabrielcostamoura1@gmail.com>
2020-04-26 00:01:18 -0300

    feat: created function(service) to handle with countries fetchs

--- src/services/api.js
+++ src/services/api.js
@@ -2,11 +2,17 @@ import axios from "axios";
 
 const url = "https://covid19.mathdro.id/api";
 
-export const fetchData = async () => {
+export const fetchData = async (country) => {
+  let changeableUrl = url;
+
+  if (country) {
+    changeableUrl = `${url}/countries/${country}`;
+  }
+
   try {
     const {
       data: { confirmed, recovered, deaths, lastUpdate },
-    } = await axios.get(url);
+    } = await axios.get(changeableUrl);
     return {
       confirmed,
       recovered,
@@ -33,10 +39,13 @@ export const fetchDailyData = async () => {
   }
 };
 
-export const countries = async () => {
+export const fetchCountries = async () => {
   try {
-    const response = await axios.get(`${url}/countries`);
-    console.log(response);
+    const {
+      data: { countries },
+    } = await axios.get(`${url}/countries`);
+    // console.log("Countries", countries);
+    return countries.map((country) => country.name);
   } catch (error) {
     console.log(error);
   }
